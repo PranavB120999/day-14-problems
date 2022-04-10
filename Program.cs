25741109f9ff79cadfa73ce4e1c750cd7b364e4c
Pranav <pranavbhople99@gmail.com>
2022-04-10 13:21:50 +0530

    main file for link list delete problem is added

--- Program.cs
+++ Program.cs
@@ -1,58 +1,18 @@
 ﻿using System;
 
-namespace StackProblem
+namespace LinkedList
 {
-    public class StackUsingLinkedlist
+    internal class Program
     {
-        private class Node
+        static void Main(string[] args)
         {
-            public int data;
-            public Node link;
-        }
-        Node top;
-        public StackUsingLinkedlist()
-        {
-            this.top = null;
-        }
-
-        public void Push(int x)
-        {
-            Node temp = new Node();
-
-            temp.data = x;
-            temp.link = top;
-            top = temp;
-        }
-
-        public void Display()
-        {
-            if (top == null)
-            {
-                Console.WriteLine("Nothing in the Stack");
-                return;
-            }
-            else
-            {
-                Console.WriteLine("The Elements of Stack Is:");
-                Node temp = top;
-                while (temp != null)
-                {
-                    Console.Write("{0} ", temp.data);
-                    temp = temp.link;
-                }
-            }
-        }
-
-        public static void Main(String[] args)
-        {
-            StackUsingLinkedlist obj = new StackUsingLinkedlist();
-
-            obj.Push(56);
-            obj.Push(30);
-            obj.Push(70);
-
-            obj.Display();
-
+            Console.WriteLine("Hello welcome to LinkedList;");
+            LinkedList list = new LinkedList();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.Pop(56);
+            list.Display();
         }
     }
 }
